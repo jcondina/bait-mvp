88889f54d7bffc0ddea4e8920c7b38341620b004
Juan I. Condina <juani.condina@hotmail.com>
2021-10-14 21:42:42 -0300

    Test staging CI

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          We are ready to go!
+          This is staging
         </p>
       </header>
     </div>
