764180b4aa93e09e091a333e7610fd31ff99bbce
Juan I. Condina <juani.condina@hotmail.com>
2021-10-19 11:20:42 -0300

    Rollback changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is staging
+          We are ready to go!
         </p>
       </header>
     </div>
